9d3d3c60f5ca3754dd728b0becd9c4a51cf6c89e
ron erlih <ronerlih@gmail.com>
2021-06-01 00:52:16 -0400

    sync s3

---
+++ js/projects.js
@@ -0,0 +1,83 @@
+
+function Project(name, img, link, blurb) {
+    this.name = name;
+    this.img = img;
+    this.link = link;
+    this.blurb = blurb;
+	 
+}
+
+	let projects = [
+	new Project('inVisible, 2017', 
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/InVisiBle/Images/+icon_glow.png', 
+					'https://onlinemuseum.youngatartmuseum.org/',
+					'iOS app, AR works'),
+	
+	new Project('Chases, 2016',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/Icon.png',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/chases-catalog.html',
+					'iOS app, AR works'),
+	
+	new Project('ATMbuddha.com, 2016',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/atmBuddha_thumb.gif',
+					'https://atmbuddha.com/',
+					'AR'),
+	
+	new Project('chaseFloyd.com, 2016',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/chaseFloyd_smallest.gif',
+					'http://chasefloyd.com/',
+					'AR'),
+	
+	new Project('exitThesaurus.com, 2016',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/exit_Go_500.jpg',
+					'http://exitthesaurus.com',
+					'AR'),
+	
+	new Project('WIreless - FIdelity, 2015',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/resources/wifi_thumb.jpg',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/RonErlih_WiFi.html',
+					'browser AR, caprive portal'),
+	
+	new Project('hold me, 2015',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/resources/holdMe05.jpg',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/RonErlih_holdMe.html',
+					'AR'),
+	
+	new Project('Kickstopper, 2015',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/3e0dfee21382f2138bd43ad500a64609_original.jpg-w%3D680%26fit%3Dmax%26v%3D1432266352%26auto%3Dformat%26q%3D92%26s%3D949cc9ea07fff9dd5e114807e0946646.jpg',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/aKickstarterCampaign/index.html',
+					'performance'),
+					
+		new Project('Alone Together, 2014',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/48591387715271.jpg',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/index_AloneTogether.html',
+					'audio to emotion to youtube'),
+
+		new Project('youTubeMirror, 2012',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/facialExpression.jpg',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/index_youTubeMirror.html',
+					'portrait to emotion to youtube'),
+
+		new Project('TwitterMirror, 2012',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/2013-09-07%2B13.07.21.jpg',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/index_twitterMirror.html',
+					'portrait to emotion to twitter'),
+
+		new Project('googleMirror, 2012',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/googleMirror_yaa_03.gif',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/index_googleMirror.html',
+					'visual similarity'),
+
+		new Project('Saturday morning, 2011',
+					'https://s3-us-west-2.amazonaws.com/ronerlihstudio.com/Images/saturday.jpg',
+					'https://s3-us-west-2.amazonaws.com/ronerlih.com/index_saturdayMorning.html',
+					'durational video 9:04')
+
+//		new Project('',
+//					'',
+//					'',
+//					'')
+
+
+	];
+	
